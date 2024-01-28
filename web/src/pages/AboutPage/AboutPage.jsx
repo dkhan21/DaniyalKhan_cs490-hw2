d825ce08ace82cd765279955bc1008cfe981fbf2
dkhan21 <dkhann17@gmail.com>
2024-01-28 02:02:25 -0500

    Initial commit

---
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,16 @@
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+
+
+      <p>
+        This site was created to demonstrate my mastery of redwood!
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
